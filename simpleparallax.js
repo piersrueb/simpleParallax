4622d053c9d8ffa45359e2f3bc6f4f19a3a5d701
piersrueb <piersrueb@gmail.com>
2018-10-03 14:35:02 +0100

    init

--- simpleparallax.js
+++ simpleparallax.js
@@ -2,11 +2,11 @@
 //  Author Piers Rueb
 //  https://github.com/piersrueb/simpleparallax
 
-function simpleParallax(id,modifier){
-	var paraId = document.getElementById('parallax-' + id);
+const simpleParallax = (id,modifier) =>{
+	let paraId = document.getElementById('parallax-' + id);
 	window.addEventListener('scroll', function(e) {
-		var x = paraId.getBoundingClientRect().top / modifier,
-			y = Math.round(x * 100) / 100;
+		let x = paraId.getBoundingClientRect().top / modifier;
+		let y = Math.round(x * 100) / 100;
 		paraId.style.backgroundPosition = 'center ' + y + 'px';
 	});
 }
