ba71afdcda35120f66783b5eba2746df498f1ccd
piersrueb <piersrueb@gmail.com>
2018-10-05 10:41:22 +0100

    bug fix, function now called on init

--- simpleparallax.js
+++ simpleparallax.js
@@ -4,9 +4,11 @@
 
 const simpleParallax = (id, modifier) => {
   	let paraId = document.querySelector(id);
-	window.addEventListener('scroll', function(e) {
-		let x = paraId.getBoundingClientRect().top / modifier;
+    const sp = () => {
+        let x = paraId.getBoundingClientRect().top / modifier;
 		let y = Math.round(x * 100) / 100;
 		paraId.style.backgroundPosition = 'center ' + y + 'px';
-	});
+    }
+    sp();
+	window.addEventListener('scroll', function(e) { sp(); });
 }
