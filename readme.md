8d493be9e78c7df5a0c22136dd845d32d4bdf397
piersrueb <piersrueb@gmail.com>
2020-10-19 15:36:55 +0100

    added support for single call across multiple classes

--- readme.md
+++ readme.md
@@ -6,18 +6,18 @@ See [demo](https://codepen.io/Rueb/pen/rZbZgj) on Codepen.
 
 ####  JS
 
-Initialise the plugin. The first argument identifies the parrallax id.
+Initialise the plugin. The first argument identifies the parrallax class.
 
 The second is the modifier it controls the parallax distance. It is defined as a fraction of the total page height and is used to animate the background image position on scroll.
 
 ```js
-simpleParallax('#parallax-1',8);
+simpleParallax('para',8);
 ```
 
 #### HTML
 
 ```html
-<section id="parallax-1"></section>
+<section class="para" id="parallax-1"></section>
 ```
 
 #### CSS
--- simpleparallax.js
+++ simpleparallax.js
@@ -2,14 +2,17 @@
 //  Author Piers Rueb
 //  https://github.com/piersrueb/simpleparallax
 
-const simpleParallax = (id, modifier) => {
-  	let paraId = document.querySelector(id);
-    paraId.setAttribute('style', 'background-repeat: no-repeat; background-attachment: fixed; background-size: cover;background-position: center center;');
-    const sp = () => {
-        let x = paraId.getBoundingClientRect().top / modifier;
-		let y = Math.round(x * 100) / 100;
-        paraId.style.backgroundPosition = 'center ' + y + 'px';
+const simpleParallax = (elem, modifier) => {
+    let paras = document.getElementsByClassName(elem);
+    for (let i = 0; i < paras.length; i++) {
+        let para = paras[i];
+        para.setAttribute('style', 'background-repeat: no-repeat; background-attachment: fixed; background-size: cover;background-position: center center;');
+        const sp = () => {
+            let x = para.getBoundingClientRect().top / modifier;
+            let y = Math.round(x * 100) / 100;
+            para.style.backgroundPosition = 'center ' + y + 'px';
+            requestAnimationFrame(sp);
+        }
         requestAnimationFrame(sp);
     }
-    requestAnimationFrame(sp);
 }
