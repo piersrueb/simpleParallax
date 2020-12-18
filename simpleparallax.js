8ee51a164a7fcbba18f4369701cb9a9131e05e4c
Piers Rueb <piersrueb@gmail.com>
2020-12-18 11:25:52 +0000

    excess request animation calls removed

--- simpleparallax.js
+++ simpleparallax.js
@@ -6,11 +6,10 @@ const simpleParallax = (elem, modifier) => {
     let paras = document.getElementsByClassName(elem);
     const sp = () => {
         for (let i = 0; i < paras.length; i++) {
-            let para = paras[i];
-            para.setAttribute('style', 'background-repeat: no-repeat; background-attachment: fixed; background-size: cover;background-position: center center;');
-            let x = para.getBoundingClientRect().top / modifier;
+            paras[i].setAttribute('style', 'background-repeat: no-repeat; background-attachment: fixed; background-size: cover;background-position: center center;');
+            let x = paras[i].getBoundingClientRect().top / modifier;
             let y = Math.round(x * 100) / 100;
-            para.style.backgroundPosition = 'center ' + y + 'px';
+            paras[i].style.backgroundPosition = 'center ' + y + 'px';
         }
         requestAnimationFrame(sp);
     }
