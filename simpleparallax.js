18f47c8f9c473d8e92a1a936af21411ac8eb977c
piersrueb <piersrueb@gmail.com>
2021-01-29 12:39:10 +0000

    style removed from animation frame call

--- simpleparallax.js
+++ simpleparallax.js
@@ -4,9 +4,11 @@
 
 const simpleParallax = (elem, modifier) => {
     let paras = document.getElementsByClassName(elem);
+    for (let i = 0; i < paras.length; i++) {
+        paras[i].setAttribute('style', 'background-repeat: no-repeat; background-attachment: fixed; background-size: cover;background-position: center center;');
+    }
     const sp = () => {
         for (let i = 0; i < paras.length; i++) {
-            paras[i].setAttribute('style', 'background-repeat: no-repeat; background-attachment: fixed; background-size: cover;background-position: center center;');
             let x = paras[i].getBoundingClientRect().top / modifier;
             let y = Math.round(x * 100) / 100;
             paras[i].style.backgroundPosition = 'center ' + y + 'px';
